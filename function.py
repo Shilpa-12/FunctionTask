06438f78cf027aaf502247826f0abbb643867b53
SHILPA SAMANTA <71275053+Shilpa-12@users.noreply.github.com>
2022-01-12 11:10:32 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+def most_frequent(string):
+    d = dict()
+    for i in sorted(string):
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+    return d
+
+print( most_frequent('mississippi'))
+
+
